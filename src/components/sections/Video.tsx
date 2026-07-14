3fbef901d37b6a4d6103d83746ed159191094b6a
MAENG YU RIM <87506702+dbfla6302@users.noreply.github.com>
2026-07-14 13:32:07 +0900

    Update Video.tsx

--- src/components/sections/Video.tsx
+++ src/components/sections/Video.tsx
@@ -5,16 +5,18 @@ import Section from '@shared/Section'
 const cx = classNames.bind(styles)
 
 function Video() {
+  const ASSET_BASE_URL = 'https://dbfla6302.github.io/Wedding-Invitation/assets'
   return (
     <Section className={cx('container')}>
       <video
         autoPlay={true}
         muted={true}
         loop={true}
-        poster="/assets/thumbnail.jpg"
+        poster={`${ASSET_BASE_URL}/poster.jpg`}
         controls={false}
       >
-        <source src="/assets/main.mp4" type="video/mp4" />
+        <source src={`${ASSET_BASE_URL}/main.webm`} type="video/webm" />
+        <source src={`${ASSET_BASE_URL}/main.mp4`} type="video/mp4" />{' '}
       </video>
     </Section>
   )
