b69aea3b01925ac897f79a2299192b2ce2a484f1
MAENG YU RIM <87506702+dbfla6302@users.noreply.github.com>
2026-07-14 11:34:29 +0900

    Update wedding.ts

--- src/api/wedding.ts
+++ src/api/wedding.ts
@@ -1,3 +1,6 @@
-export function getWedding() {
-  return fetch('http://localhost:8888/wedding')
+const WEDDING_DATA_URL =
+  'https://raw.githubusercontent.com/dbfla6302/Wedding-Invitation/main/db.json'
+
+export function getWedding(): Promise<Response> {
+  return fetch(WEDDING_DATA_URL)
 }
